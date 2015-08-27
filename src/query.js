e183128795104d147e55264ca6f9663790e0bab4
farcek <farcek@yahoo.com>
2015-08-27 17:30:16 +0900

    fix

--- src/query.js
+++ src/query.js
@@ -2,9 +2,10 @@ var Promise = require('bluebird');
 var _ = require('lodash');
 var helper = require('./helper');
 
-function Query(schema, knex) {
+function Query(schema, knex, $$q) {
     this.$schema = schema;
     this.$knex = knex;
+    this.$$q = $$q;
 
     ;
 }
@@ -80,32 +81,138 @@ Query.prototype.$columns = function () {
     return cols;
 }
 
+//<editor-fold desc="where group">
+Query.prototype.$where = function (method, params) {
+
+    var self = this;
+    var rq = this.from();
+    var fn = rq[method];
+    if (fn && _.isFunction(fn)) {
+        if (params.length === 1) {
+            var p = params[0]
+            if (p === false || p === true) {
+                fn.call(rq, p);
+                return self
+            }
+
+            if (_.isFunction(p)) {
+                fn.call(rq, function () {
+                    var sq = new Query(self.schema(), self.knex(), this)
+                    p.call(sq);
+                });
+
+                return self
+            }
+
+
+            if (_.isPlainObject(p)) {
+                for (var k in p) self.$where(method, [k, p[k]]);
+                return self
+            }
+            throw 'todo'
+        }
+        if (params.length === 2) {
+
+
+            var col = params[0];
+            if (_.isString(col)) {
+                col = self.$column(col, true);
+            }
+
+
+            fn.call(rq, col, params[1])
+            return self
+        }
+        if (params.length === 3) {
+
+
+            var col = params[0];
+            if (_.isString(col)) {
+                col = self.$column(col, true);
+            }
+
+
+            fn.call(rq, col, params[1], params[2])
+            return self
+        }
+
+        throw new Error('bad request');
+
+    }
+    throw new Error('not fund function. find function name ' + method);
+}
 
 Query.prototype.where = function (field, op, value) {
-    var self = this;
-
-    // object where
-    if (arguments.length === 1 && _.isObject(field)) {
-        for (var k in field) self.where(k, field[k]);
-        return self;
-    }
-    //if (field instanceof Raw && arguments.length === 1) return this.whereRaw(column);
-    // todo raw
-
-
-    var col = self.$column(field, true);
-
-
-    if (arguments.length === 2) {
-        value = op;
-        op = '=';
-    }
-
-    var q = this.from();
-    q.where(col, op, value);
-
-    return self;
+    return this.$where('where', arguments)
 }
+Query.prototype.orWhere = function (field, op, value) {
+    return this.$where('orWhere', arguments)
+}
+
+Query.prototype.whereNot = function (field, op, value) {
+    return this.$where('whereNot', arguments)
+}
+Query.prototype.orWhereNot = function (field, op, value) {
+    return this.$where('orWhereNot', arguments)
+}
+
+Query.prototype.whereIn = function (field, op, value) {
+    return this.$where('whereIn', arguments)
+}
+Query.prototype.orWhereIn = function (field, op, value) {
+    return this.$where('orWhereIn', arguments)
+}
+
+Query.prototype.whereNotIn = function (field, op, value) {
+    return this.$where('whereNotIn', arguments)
+}
+Query.prototype.orWhereNotIn = function (field, op, value) {
+    return this.$where('orWhereNotIn', arguments)
+}
+
+Query.prototype.whereNull = function (field, op, value) {
+    return this.$where('whereNull', arguments)
+}
+Query.prototype.orWhereNull = function (field, op, value) {
+    return this.$where('orWhereNull', arguments)
+}
+
+Query.prototype.whereNotNull = function (field, op, value) {
+    return this.$where('whereNotNull', arguments)
+}
+Query.prototype.orWhereNotNull = function (field, op, value) {
+    return this.$where('orWhereNotNull', arguments)
+}
+
+Query.prototype.whereExists = function (field, op, value) {
+    return this.$where('whereExists', arguments)
+}
+Query.prototype.orWhereExists = function (field, op, value) {
+    return this.$where('orWhereExists', arguments)
+}
+
+Query.prototype.whereExists = function (field, op, value) {
+    return this.$where('whereExists', arguments)
+}
+Query.prototype.orWhereExists = function (field, op, value) {
+    return this.$where('orWhereExists', arguments)
+}
+
+Query.prototype.whereNotExists = function (field, op, value) {
+    return this.$where('whereNotExists', arguments)
+}
+Query.prototype.orWhereNotExists = function (field, op, value) {
+    return this.$where('orWhereNotExists', arguments)
+}
+
+Query.prototype.whereBetween = function (field, op, value) {
+    return this.$where('whereBetween', arguments)
+}
+Query.prototype.orWhereBetween = function (field, op, value) {
+    return this.$where('orWhereBetween', arguments)
+}
+//</editor-fold>
+
 
 Query.prototype.resultRaw = function (callback) {
     var self = this;
@@ -166,6 +273,7 @@ Query.prototype.insert = function (data, returning) {
     me.from().insert(data, returning);
     return me;
 }
+
 Query.prototype.limit = function (value) {
     var me = this;
     me.from().limit(value)
@@ -190,6 +298,7 @@ Query.prototype.orderBy = function (field, direction) {
     me.from().orderBy(col, direction);
     return me;
 }
+
 Query.prototype.transacting = function (trx) {
     var me = this;
 
@@ -212,13 +321,24 @@ Query.prototype.count = function (callback) {
 
 }
 
+// todo
+//Query.prototype.join = function (schema, first, operator, second) {
+//    var self = this;
+//    var joinSchema = self.schema().container().schema(schema);
+//    self.from().join(joinSchema.dbName(), first, operator, second)
+//
+//    return self;
+//}
 
-Query.prototype.join = function (schema, first, operator, second) {
+Query.prototype.toSQL = function () {
     var self = this;
-    var joinSchema = self.schema().container().schema(schema);
-    self.from().join(joinSchema.dbName(), first, operator, second)
-
-    return self;
+    return self.from().toSQL()
 }
+
+Query.prototype.toString = function () {
+    var self = this;
+    return self.from().toString()
+}
+
 
 module.exports = Query;
---
+++ test/query01.js
@@ -0,0 +1,57 @@
+var assert = require("assert");
+var utils = require('util')
+var FDB = require('../src/container')
+var Field = require('../src/field')
+
+
+describe('query', function () {
+
+    var fdb = new FDB(require('./conn'));
+
+
+    var User = fdb.define('user', {
+        id: {
+            type: 'integer',
+            pk: 1, gen: 1, dbName: 'kk'
+
+        },
+        user: String
+
+    }, {
+        dbName: 'user_query'
+    })
+
+
+    var user = 'farcek ' + new Date();
+
+
+    before(function () {
+        return fdb.knex().schema.dropTableIfExists(User.dbName())
+
+            .then(function () {
+                return User.init()
+            })
+
+    });
+
+
+    describe.only('query', function () {
+        it('or where', function () {
+
+            var q = User.select();
+            q.where('id',15)
+            q.where(function (sq) {
+                this.where('id', 1).orWhere('id', '>', 10)
+            })
+
+
+
+            assert.equal('select * from `user_query` where `kk` = 15 and (`kk` = 1 or `kk` > 10)', q.toString())
+
+        });
+
+
+    });
+
+
+});
